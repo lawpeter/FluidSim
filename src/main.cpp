afdfa7cb0492bb460b41b844a9001feb6d2b7d4c
Owen Poole <owen.poole@live.com>
2025-07-17 14:19:05 -0700

    Commented code

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ GLFWwindow* loadSim()
 {
     if (!glfwInit())
     {
-        std::cout << "brokey" << std::endl;
+        std::cout << "failed to initialize glfw" << std::endl;
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
@@ -59,13 +59,13 @@ GLFWwindow* loadSim()
 
     if (window == NULL)
     {
-        std::cout << "brokey 2" << std::endl;
+        std::cout << "could not create window" << std::endl;
     }
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "brokey 3" << std::endl;
+        std::cout << "failed to load glad" << std::endl;
     }
 
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -148,6 +148,7 @@ float nearDensityKernelDerivative(float distance)
 
 int positionToCellArrayIndex(glm::vec2 position)
 {
+    // makes a grid based on smoothing radius
     int cellX = (int) (position.x / smoothingRadius);
     int cellY = (int) (position.y / smoothingRadius);
 
@@ -160,6 +161,7 @@ void checkNearbyParticles(std::vector<int>& results, std::vector<int>& cellOffse
 
     int cell = positionToCellArrayIndex(particle.position);
 
+    // Check all nearby grid cells and add their particles to the results array if they are within the grid
     for (int offset : cellOffsets)
     {
         int cellToCheck = cell + offset;
@@ -183,6 +185,7 @@ void calculateDensities(std::vector<Particle>& particles, std::vector<Particle>&
         float density = 0.0f;
         float nearDensity = 0.0f;
 
+        // Populates the results array with nearby particles to check
         checkNearbyParticles(results, cellOffsets, predictedParticles[currentParticleIndex], predictedParticles, grid);
 
         for (int otherParticleIndex : results)
@@ -191,6 +194,7 @@ void calculateDensities(std::vector<Particle>& particles, std::vector<Particle>&
             glm::vec2 offsetToOtherParticle = otherParticlePosition - currentParticlePosition;
             float sqrDistanceToOtherParticle = glm::dot(offsetToOtherParticle, offsetToOtherParticle);
 
+            // Skip particle if its outside the smoothing radius
             if (sqrDistanceToOtherParticle > pow(smoothingRadius, 2)) continue;
 
             float distance = sqrt(sqrDistanceToOtherParticle);
@@ -201,6 +205,7 @@ void calculateDensities(std::vector<Particle>& particles, std::vector<Particle>&
             nearDensity += mass * nearInfluence;
         }
 
+        // Add calculated density for the particle to the densities array (with the same index as the particle)
         densities.push_back(density);
         nearDensities.push_back(nearDensity);
     }
@@ -230,14 +235,17 @@ glm::vec2 calculatePressureForce(int particleIndex, std::vector<Particle>& parti
 
     int maxIndex = 0;
 
+    // Check all particles within nearby grid cell (using previosly populated results array)
     for (int otherParticleIndex : results) 
     {
+        // Skip if checking itself
         if (otherParticleIndex == particleIndex) continue;
         glm::vec2 otherParticlePosition = predictedParticles[otherParticleIndex].position;
         
         glm::vec2 offsetToOtherParticle = otherParticlePosition - currentParticlePosition;
         float sqrDistanceToOtherParticle = glm::dot(offsetToOtherParticle, offsetToOtherParticle);
         
+        // Skip if outside smoothing radius
         if (sqrDistanceToOtherParticle > pow(smoothingRadius, 2)) continue;
 
         float distance = sqrt(sqrDistanceToOtherParticle);
@@ -249,6 +257,7 @@ glm::vec2 calculatePressureForce(int particleIndex, std::vector<Particle>& parti
         float otherPressure = convertDensityToPressure(otherDensity);
         float otherNearPressure = convertNearDensityToPressure(otherNearDensity);
     
+        // Apply the average of the two pressures to each particle
         float sharedPressure = (currentPressure + otherPressure) * 0.5f;
         float sharedNearPressure = (currentNearPressure + otherNearPressure) * 0.5f;
 
@@ -271,8 +280,11 @@ glm::vec2 calculateViscosity(int particleIndex, std::vector<Particle>& particles
 {
     glm::vec2 viscosityForce(0, 0);
     glm::vec2 currentParticlePosition = predictedParticles[particleIndex].position;
+
+    // Check all particles in nearby grid cells (using previously populated results array)
     for (int otherParticleIndex : results)
     {
+        // Skip if checking itself
         if (otherParticleIndex == particleIndex) continue;
 
         glm::vec2 otherParticlePosition = predictedParticles[otherParticleIndex].position;
@@ -280,6 +292,7 @@ glm::vec2 calculateViscosity(int particleIndex, std::vector<Particle>& particles
         glm::vec2 offsetToOtherParticle = otherParticlePosition - currentParticlePosition;
         float sqrDistanceToOtherParticle = glm::dot(offsetToOtherParticle, offsetToOtherParticle);
 
+        // Skip if other particle is outside smoothing radius
         if (sqrDistanceToOtherParticle > pow(smoothingRadius, 2)) continue;
         
         glm::vec2 otherVelocity = particles[otherParticleIndex].velocity;
@@ -296,6 +309,7 @@ void updateParticleCells(std::vector<Particle>& particles, std::vector<std::vect
 
     for (int i = 0; i < particles.size(); i++)
     {
+        // Calculate one dimensional array index from position, and add particle to respective grid cell if it is within bounds
         int cellIndex = positionToCellArrayIndex(particles[i].position);
         if (cellIndex < grid.size() && cellIndex >= 0) 
         {
@@ -310,6 +324,7 @@ void updateParticleCells(std::vector<Particle>& particles, std::vector<std::vect
 
 void constructCellOffsets(std::vector<int>& cellOffsets)
 {
+    // Construct array containing all necessary indices to check 3x3 grid of nearby cells
     cellOffsets.push_back(-1);
     cellOffsets.push_back(0);
     cellOffsets.push_back(1);
@@ -376,8 +391,10 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 
 int main(int argc, char* argv[])
 {
+    // Orthographic projection for shaders
     glm::mat4 projection = glm::ortho(0.0f, SCREEN_WIDTH, 0.0f, SCREEN_HEIGHT, -1.0f, 1.0f);
 
+    // Initialize window to render
     GLFWwindow* window = loadSim();
 
     if (window == NULL)
@@ -385,12 +402,10 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    // Initialize settings control window
     ImGui::CreateContext();
     ImGui_ImplGlfwGL3_Init(window, true);
     ImGui::StyleColorsDark();
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     GLuint VAO;
     GLuint quadVBO;
@@ -398,14 +413,18 @@ int main(int argc, char* argv[])
     GLuint particleVBO;
     GLuint instanceVBO;
 
+    // Call method that creates and compiles shaders with shader sources that are #included from external files
     GLuint shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
 
+    // Place projection matrix inside shader uniform
     GLint projectionUniformLocation = glGetUniformLocation(shaderProgram, "projection");
     glUniformMatrix4fv(projectionUniformLocation, 1, GL_FALSE, &projection[0][0]);
 
+    // Place radius inside shader uniform
     GLint radiusUniformLocation = glGetUniformLocation(shaderProgram, "radius");
     glUniform1f(radiusUniformLocation, Particle::radius);
 
+    // Construct the corners of our quad used for rendering
     float quadVertices[8] = {
         -Particle::radius, -Particle::radius, // 0
          Particle::radius, -Particle::radius, // 1
@@ -413,6 +432,7 @@ int main(int argc, char* argv[])
         -Particle::radius,  Particle::radius  // 3
     };
 
+    // Construct list of index order used to decide triangles for quad rendering
     GLuint indices[6] = {
         0, 1, 2,
         2, 3, 0
@@ -421,6 +441,7 @@ int main(int argc, char* argv[])
     std::vector<Particle> particles;
     initializeParticles(particles);
 
+    // Pre-size grid array to the max size
     std::vector<std::vector<int>> grid;
     grid.resize(gridWidth * gridHeight);
 
@@ -443,7 +464,7 @@ int main(int argc, char* argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GLuint), indices, GL_STATIC_DRAW);
 
-    // Mesh position
+    // Assign quad position instructions for vertex shader at layout position 0
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
@@ -451,22 +472,25 @@ int main(int argc, char* argv[])
     glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
     glBufferData(GL_ARRAY_BUFFER, particles.size() * sizeof(Particle), particles.data(), GL_DYNAMIC_DRAW);
 
+    // Shader's step size when rendering particles
     GLsizei stride = sizeof(Particle);
 
-    // particlePosition > location 1, vec2
+    // Assigns vec2 particle position data to particlePosition in shader at location 1
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)offsetof(Particle, position));
     glEnableVertexAttribArray(1);
     glVertexAttribDivisor(1, 1);
 
-    // particleVelocity > location 2, vec2
+    // Assigns vec2 particle velocity data to particleVelocity in shader at location 2
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)offsetof(Particle, velocity));
     glEnableVertexAttribArray(2);
     glVertexAttribDivisor(2, 1);
 
+    // Initialize variables used for deltaTime and average FPS
     double lastTime = glfwGetTime();
     int timeCount = 0;
     float deltaTimeSum = 0.0f;
 
+    // Put in positions to predicted particle array (used for calculations)
     std::vector<Particle> predictedParticles;
     for (Particle p : particles)
     {
@@ -477,8 +501,10 @@ int main(int argc, char* argv[])
     glfwSetCursorPosCallback(window, cursor_position_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
 
+    // Runs every frame until it is closed
     while (!glfwWindowShouldClose(window))
     {
+        // Updates deltaTime and average FPS
         double nowTime = glfwGetTime();
         float deltaTime = (float) nowTime - lastTime;
         lastTime = nowTime;
@@ -488,9 +514,7 @@ int main(int argc, char* argv[])
         timeCount++;
 
         // Keypress handling
-        if (commaPressed)
-            frameCount++;
-        
+        if (commaPressed) frameCount++;
         if (spacePressed)
         {
             deltaTime *= 0;
@@ -505,7 +529,6 @@ int main(int argc, char* argv[])
                 periodPressed = false;
             }
         }
-
         if (commaPressed)
         {
             if (frameCount < 10)
@@ -517,7 +540,6 @@ int main(int argc, char* argv[])
                 commaPressed = false;
             }
         }
-
         if (rPressed)
         {
             particles.clear();
@@ -525,26 +547,27 @@ int main(int argc, char* argv[])
             rPressed = false;
         }
 
-
-
         // Clear screen each frame
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         ImGui_ImplGlfwGL3_NewFrame();
 
+        // Clear densities arrays to get ready for new calculations
         densities.clear();
         nearDensities.clear();
 
-        
+        // Add gravity acceleration and update predicted particles to new predicted positions
         for (int i = 0; i < particles.size(); i++)
         {
             particles[i].accelerate(glm::vec2(0.0f, -gravity * deltaTime));
             predictedParticles[i].position = particles[i].position + particles[i].velocity * deltaTime;
         }
         
+        // Repopulate grid cells with particles
         updateParticleCells(predictedParticles, grid);
 
+        // Calculate all densities associated with each particle
         calculateDensities(particles, predictedParticles, densities, nearDensities, results, cellOffsets, grid);
 
         for (int i = 0; i < particles.size(); i++)
@@ -562,7 +585,7 @@ int main(int argc, char* argv[])
             glm::vec2 viscosityAcceleration = viscosityForce / glm::max(densities[i], 1e-4f);
             particles[i].accelerate(viscosityAcceleration * deltaTime);
 
-            // Mouse interaction
+            // Mouse interaction forces
             if (leftMousePressed || rightMousePressed)
             {
                 glm::vec2 cursorPosition(cursorX, SCREEN_HEIGHT - cursorY);
@@ -578,11 +601,13 @@ int main(int argc, char* argv[])
                     
                     if (leftMousePressed)
                     {
-                        particles[i].accelerate(-normalizedDireciton * strength * deltaTime);
+                        // Pull toward from mouse position (due to positive)
+                        particles[i].accelerate(normalizedDireciton * strength * deltaTime);
                     }
                     if (rightMousePressed)
                     {
-                        particles[i].accelerate(normalizedDireciton * strength * deltaTime);
+                        // Push away from mouse position (due to negative)
+                        particles[i].accelerate(-normalizedDireciton * strength * deltaTime);
                     }
                 }
             }
@@ -590,21 +615,26 @@ int main(int argc, char* argv[])
 
         for (int i = 0; i < particles.size(); i++)
         {
+            // Move each particle due to velocity
             particles[i].updatePosition(deltaTime);
 
             int gridIndex = positionToCellArrayIndex(particles[i].position);
 
+            // Only check edge grids to see if they're outside bounds and change direction (due to collision)
             if (gridIndex < gridWidth || gridIndex > gridWidth * (gridHeight - 1) || gridIndex % (int) gridWidth == 0 || gridIndex % (int) gridWidth == gridWidth - 1)
             {
                 particles[i].doCollison();
             }
         }
 
+        // Add new data to buffer array for shaders
         glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
         glBufferData(GL_ARRAY_BUFFER, particles.size()*sizeof(Particle), particles.data(), GL_DYNAMIC_DRAW);
         
+        // Draw particles using instanced data from quads
         glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr, numRows * numColumns);
 
+        // Set settings to be controlled using settings window
         ImGui::Begin("Controls");
         ImGui::SliderFloat("PressureMultiplier", &pressureMultiplier, 0.0f, 50.0f);
         ImGui::SliderFloat("nearPressureMultiplier", &nearPressureMultiplier, 0.0f, 100.0f);
@@ -624,6 +654,7 @@ int main(int argc, char* argv[])
 
     std::cout << "Average FPS: " << 1 / (deltaTimeSum / timeCount) << " " << timeCount << std::endl;
 
+    // Cleanup
     ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();
     glfwTerminate();
